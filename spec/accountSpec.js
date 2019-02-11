4883c69ad9c156d6b77d2a987d804cb7e09bee84
Jethro Whaley <jethro.whaley@gmail.com>
2019-02-11 15:21:07 +0000

    deposits returns amount deposited

--- spec/accountSpec.js
+++ spec/accountSpec.js
@@ -7,9 +7,16 @@ describe("Account", () => {
 
   describe("#deposit", () => {
 
-  it("a user can add a deposit", () => {
-    account.deposit(1000);
-    expect(account.deposits).toBeGreaterThan(0);
+    it("a user can add a deposit", () => {
+      account.deposit(1000);
+      expect(account.deposits).toBeGreaterThan(0);
     })
 
+    it("tells you the deposit amount", () => {
+      account.deposit(1000);
+      account.deposit(500);
+      expect(account.deposits[0]).toBe(1000);
+      expect(account.deposits[1]).toBe(500);
+    })
+  });
 });
